ba1d9c80436e73b7a7049e9976f02334666774f5
J-I-H-O <jeongjiho0731@gmail.com>
2023-10-07 12:29:10 +0900

    [TDD]: initial test

--- DB/src/test/java/JdbcTest.java
+++ DB/src/test/java/JdbcTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
 
 public class JdbcTest {
 
@@ -10,19 +12,43 @@ public class JdbcTest {
         String path = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/jdbcTest";
 
+        Instant start = null;   // 성능 측정 시작 시각
+        Instant end = null;     // 성능 측정 종료 시각
+        
         try {
+            // ======================측정 시작===================== //
+            start = Instant.now();
+
             // 1. JDBC 드라이버 로딩
             Class.forName(path);
-            
+
             // 2. Connection 객체 생성
-            Connection conn = DriverManager.getConnection(url, USERNAME, PASSWORD);
+            Connection conn = DriverManager.getConnection(url, "user", "password");
+
+            end = Instant.now();
+            System.out.println("Connection 수행 시간: " + Duration.between(start, end).toMillis() + "ms");
+            // ======================측정 종료===================== //
+
+            // ======================측정 시작===================== //
+            start = Instant.now();
 
             // 3. Statement 객체 생성
             Statement statement = conn.createStatement();
 
+            end = Instant.now();
+            System.out.println("Statement 생성 수행 시간: " + Duration.between(start, end).toMillis() + "ms");
+            // ======================측정 종료===================== //
+
+            // ======================측정 시작===================== //
+            start = Instant.now();
+
             // 4. SELECT 쿼리 실행
             String query = "SELECT * FROM user";
             ResultSet result = statement.executeQuery(query);
+
+            end = Instant.now();
+            System.out.println("Query 수행 시간: " + Duration.between(start, end).toMillis() + "ms");
+            // ======================측정 종료===================== //
 
             // 5. 쿼리 실행 결과 처리
             while(result.next()) {
